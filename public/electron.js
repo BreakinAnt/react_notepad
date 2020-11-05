3cbed548ae17d0b85ef9c64b2cc770ebaf9659f7
Felipe Carneiro <felipesan08@hotmail.com>
2020-11-05 19:29:26 -0300

    removed menubar

--- public/electron.js
+++ public/electron.js
@@ -8,9 +8,10 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680, show: false, resizable: false, fullscreen: false });
+  mainWindow = new BrowserWindow({width: 900, height: 680, show: false, resizable: false, fullscreen: false});
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.removeMenu();
   mainWindow.once('ready-to-show', () => {
     mainWindow.show()
   })
